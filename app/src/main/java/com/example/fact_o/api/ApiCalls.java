e07f5abec649004e1368c95dfbe13ab2d258ea03
Sudarshan <sudarshandevkota22@gmail.com>
2022-06-01 20:39:44 +0545

    model class and api client implemented

---
+++ app/src/main/java/com/example/fact_o/api/ApiCalls.java
@@ -0,0 +1,15 @@
+package com.example.fact_o.api;
+
+import com.example.fact_o.model.Fact;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+
+public interface ApiCalls {
+    @Headers("X-Api-Key: cwYa40HzV+aO7BLiUJX9Mg==umPhIpiIPUmCrDUy")
+    @GET("/v1/facts?limit=30")
+    Call<List<Fact>> getFacts();
+}
---
+++ app/src/main/java/com/example/fact_o/model/Fact.java
@@ -0,0 +1,14 @@
+package com.example.fact_o.model;
+
+public class Fact {
+
+    String fact;
+
+    public String getFact() {
+        return fact;
+    }
+
+    public void setFact(String fact) {
+        this.fact = fact;
+    }
+}
