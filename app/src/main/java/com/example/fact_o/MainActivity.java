dde1f0d7ee4a725323c31c5e159f611ab24e666d
Sudarshan <sudarshandevkota22@gmail.com>
2022-06-05 08:45:19 +0545

    infinite loop and data loading implemented
    	- also orientation locked to portrait

--- app/src/main/java/com/example/fact_o/MainActivity.java
+++ app/src/main/java/com/example/fact_o/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
 
 import com.example.fact_o.adapter.RecyclerAdapter;
 import com.example.fact_o.api.ApiCalls;
 import com.example.fact_o.client.FactClient;
 import com.example.fact_o.model.Fact;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -19,16 +22,40 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
-    ViewPager2 viewPager ;
+    ViewPager2 viewPager;
+    RecyclerAdapter adapter;
+    List<Fact> factList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         viewPager = findViewById(R.id.view_pager);
 
+        factList = new ArrayList<>();
+        adapter = new RecyclerAdapter(factList,this);
+        viewPager.setAdapter(adapter);
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                Log.d("TAG", "onPageSelected: "+position);
+                if(position%30==28){
+                    loadNewData();
+                }
+
+            }
+        });
+        loadNewData();
 
 
+    }
+    private void generateList(List<Fact> list){
+        Log.d("TAG", "generateList: content loaded");
+        factList.addAll(list);
+        adapter.notifyDataSetChanged();
+
+    }
+    private void loadNewData(){
         ApiCalls api = FactClient.getInstance().create(ApiCalls.class);
         Call<List<Fact>> call = api.getFacts();
         call.enqueue(new Callback<List<Fact>>() {
@@ -42,13 +69,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
-    }
-    private void generateList(List<Fact> list){
-        Log.d("TAG", "generateList: content loaded");
-        viewPager.setAdapter(new RecyclerAdapter(list,this));
-
     }
 
 }
